1e7c9400a7cb925b1c58959d65449bffd5be896b
Simon WEBER <simon_weber@carrefour.com>
2020-06-22 14:09:39 +0200

    Adjust postgre database connexion settings

--- src/main/java/com/descartes/qlf/QlfApplication.java
+++ src/main/java/com/descartes/qlf/QlfApplication.java
@@ -2,10 +2,14 @@ package com.descartes.qlf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 // Ceci est une application Spring
 // Un deuxième commentaire
 @SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.descartes.qlf.model"})
 public class QlfApplication {
 
   public static void main(String[] args) {
