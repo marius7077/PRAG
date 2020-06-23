c3a87374c2563834a67597d7770b1aaa00648498
marius707 <marius707@hotmail.fr>
2020-06-23 10:17:20 +0100

    CLogin v1.1 First Login link between QlfController, index.html and greeting.html

---
+++ src/main/java/com/descartes/qlf/controller/AuthentificationService.java
@@ -0,0 +1,11 @@
+package com.descartes.qlf.controller;
+
+public class AuthentificationService() {
+    String username, String password;
+
+    public AuthentificationService(java.lang.String username, java.lang.String string) {
+        this.username = username;
+        String = string;
+    }
+    
+}
--- src/main/java/com/descartes/qlf/controller/QlfController.java
+++ src/main/java/com/descartes/qlf/controller/QlfController.java
@@ -3,6 +3,8 @@ package com.descartes.qlf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -13,6 +15,20 @@ public class QlfController {
       @RequestParam(name = "name", required = false, defaultValue = "World") String name,
       Model model) {
     model.addAttribute("name", name);
+    System.out.println("controllergreeting");
     return "greeting";
   }
-}
+  @RequestMapping(value = "login", method = RequestMethod.POST)
+  public String login(
+          @RequestParam(name = "txtLogin", required = true, defaultValue = "World") String login,
+          @RequestParam(name = "txtPassword", required = true, defaultValue = "World") String password,
+
+          Model model) {
+    model.addAttribute("txtLogin", login);
+    model.addAttribute("txtPassword", password);
+    System.out.println("controllerlogin," + login);
+    return "greeting";
+  }
+  }
+
+
