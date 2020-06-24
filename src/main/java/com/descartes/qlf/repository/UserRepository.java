2a7a2956b398e9b4e96c64316692b6efc8031c98
marius707 <marius707@hotmail.fr>
2020-06-24 10:15:19 +0100

    Add html security email

--- src/main/java/com/descartes/qlf/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.descartes.qlf.repository;
-
-import com.descartes.qlf.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-    User findByEmail(String email);
-}
