e721a312a4854033964f9fb9cdc163acc047f5fc
Simon WEBER <simon_weber@carrefour.com>
2020-06-24 11:25:22 +0200

    Fix ProductCategory structure

--- src/main/java/com/descartes/qlf/model/ProductCategory.java
+++ src/main/java/com/descartes/qlf/model/ProductCategory.java
@@ -11,14 +11,12 @@ public class ProductCategory implements Serializable {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private long id;
 
-  private int category;
   private String name;
 
   public ProductCategory() {}
 
-  public ProductCategory(long id, int category, String name) {
+  public ProductCategory(long id, String name) {
     this.id = id;
-    this.category = category;
     this.name = name;
   }
 
@@ -30,14 +28,6 @@ public class ProductCategory implements Serializable {
 
   public void setId(long id) {
     this.id = id;
-  }
-
-  public int getCategory() {
-    return category;
-  }
-
-  public void setCategory(int category) {
-    this.category = category;
   }
 
   public String getName() {
