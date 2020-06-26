c5256b855b4325daed3475be019b4b99d3a7c5d0
minhtri2309 <le.minhtri230999@gmail.com>
2020-06-26 17:22:04 +0200

    mdp-oublie : envoie de mail pour la récupération de mot de passe

--- src/main/java/com/descartes/qlf/configuration/QlfConfiguration.java
+++ src/main/java/com/descartes/qlf/configuration/QlfConfiguration.java
@@ -5,9 +5,12 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 public class QlfConfiguration {
@@ -41,5 +44,21 @@ public class QlfConfiguration {
   @Bean
   public BCryptPasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder(12);
+  }
+
+  @Bean
+  public JavaMailSender getJavaMailSender() {
+    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+    mailSender.setHost("smtp.gmail.com");
+    mailSender.setPort(587);
+    mailSender.setUsername("quelaferme@gmail.com");
+    mailSender.setPassword("prag2020");
+    Properties props = mailSender.getJavaMailProperties();
+    props.put("mail.transport.protocol", "smtp");
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.debug", "true");
+    props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+    return mailSender;
   }
 }
